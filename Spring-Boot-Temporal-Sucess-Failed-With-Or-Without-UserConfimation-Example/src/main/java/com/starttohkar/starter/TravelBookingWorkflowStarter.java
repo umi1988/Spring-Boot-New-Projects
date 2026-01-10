35a39bd14db27080967cff848ce25dc1d2120bb7
umeshkumar <umesh.se1988@gmail.com>
2026-01-10 10:43:31 +0530

    Spring boot Spring-Boot-Temporal-Sucess-Failed-With-Or-Without-UserConfimation-Example

---
+++ Spring-Boot-Temporal-Sucess-Failed-With-Or-Without-UserConfimation-Example/src/main/java/com/starttohkar/starter/TravelBookingWorkflowStarter.java
@@ -0,0 +1,41 @@
+package com.starttohkar.starter;
+
+import com.starttohkar.dto.TravelRequest;
+import com.starttohkar.worker.TravelWorkflow;
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TravelBookingWorkflowStarter {
+
+    @Autowired
+    private WorkflowServiceStubs serviceStubs;
+
+
+    public void startWorkFlow(TravelRequest travelRequest){
+        WorkflowClient client = WorkflowClient.newInstance(serviceStubs);
+
+        TravelWorkflow workflow = client.newWorkflowStub(
+                TravelWorkflow.class,
+                WorkflowOptions.newBuilder()
+                        .setTaskQueue("TRAVEL_TASK_QUEUE")
+                        .setWorkflowId("travel_" + travelRequest.getUserId())
+                        .build()
+        );
+
+        WorkflowClient.start(workflow::bookTrip, travelRequest);
+    }
+
+
+    public void sendConfirmationSignal(String userId) {
+        WorkflowClient client = WorkflowClient.newInstance(serviceStubs);
+
+        String workflowId = "travel_" + userId;
+        TravelWorkflow workflow = client.newWorkflowStub(TravelWorkflow.class, workflowId);
+
+        workflow.sendConfirmationSignal();
+    }
+}
